21b45ae5246dbcbb92f14ba7e15a51a1fc04dea0
dharmahermanda-001 <dharma_hermanda@kamoro.com>
2025-11-06 09:09:30 +0700

    Update generate_tokens.dart

--- tool/generate_tokens.dart
+++ tool/generate_tokens.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 void main() async {
   final inputPath = 'tokens/tokens.json';
   final tokenOutputPath = 'packages/fmi_core/lib/design_tokens/mds_tokens.dart';
-  final componentBasePath ='packages/fmi_core/lib/components';
 
   final inputFile = File(inputPath);
   if (!inputFile.existsSync()) {
